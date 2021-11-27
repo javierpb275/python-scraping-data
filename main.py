f4d0320c041d08b51fc80af966d7730ec9e0753d
javierpb275 <javierpb275@gmail.com>
2021-11-27 19:12:27 +0100

    hackeer news project 2: getting also de votes and only getting the ones with points over 100 (pprint: for printing prettier)

--- main.py
+++ main.py
@@ -1,21 +1,23 @@
 import requests
 from bs4 import BeautifulSoup
+import pprint
 
 if __name__ == '__main__':
     res = requests.get('https://news.ycombinator.com/news')
     soup = BeautifulSoup(res.text, 'html.parser')
     links = soup.select('.titlelink')
-    votes = soup.select('.score')
-    print(votes[0].get('id'))
+    subtext = soup.select('.subtext')
 
-    def create_custom_hn(links, votes):
+    def create_custom_hn(links, subtext):
         hn = []
         for idx, item in enumerate(links):
-            title = links[idx].getText()
-            href = links[idx].get('href', None)
-            points = int(votes[idx].getText().replace(' points', ''))
-            print(points)
-            hn.append({'title': title,'link':href})
+            title = item.getText()
+            href = item.get('href', None)
+            vote = subtext[idx].select('.score')
+            if len(vote):
+                points = int(vote[0].getText().replace(' points', ''))
+                if points > 99:
+                    hn.append({'title': title,'link':href, 'votes': points})
         return hn
 
-    print(create_custom_hn(links, votes))
+    pprint.pprint(create_custom_hn(links, subtext))
