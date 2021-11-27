96245ca2bee3c5f1e020dece419b7354e2323e13
javierpb275 <javierpb275@gmail.com>
2021-11-27 18:58:05 +0100

    hackeer news project: created create_custom_hn function to display list of news with title and link. getting also list of points for each news

--- main.py
+++ main.py
@@ -7,3 +7,15 @@ if __name__ == '__main__':
     links = soup.select('.titlelink')
     votes = soup.select('.score')
     print(votes[0].get('id'))
+
+    def create_custom_hn(links, votes):
+        hn = []
+        for idx, item in enumerate(links):
+            title = links[idx].getText()
+            href = links[idx].get('href', None)
+            points = int(votes[idx].getText().replace(' points', ''))
+            print(points)
+            hn.append({'title': title,'link':href})
+        return hn
+
+    print(create_custom_hn(links, votes))
