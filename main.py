dc4c1fb9b2d7faaea74f1fd8f62c441f7d555173
javierpb275 <javierpb275@gmail.com>
2021-11-27 18:43:21 +0100

    beautifulsoup selectors

--- main.py
+++ main.py
@@ -4,4 +4,6 @@ from bs4 import BeautifulSoup
 if __name__ == '__main__':
     res = requests.get('https://news.ycombinator.com/news')
     soup = BeautifulSoup(res.text, 'html.parser')
-    print(soup.find('div'))
+    links = soup.select('.titlelink')
+    votes = soup.select('.score')
+    print(votes[0].get('id'))
