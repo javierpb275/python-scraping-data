d343741f9d9fb2a33bb007dd3d0d5d777a1963c4
javierpb275 <javierpb275@gmail.com>
2021-11-27 18:34:04 +0100

    beautifulsoup basics

--- main.py
+++ main.py
@@ -3,4 +3,5 @@ from bs4 import BeautifulSoup
 
 if __name__ == '__main__':
     res = requests.get('https://news.ycombinator.com/news')
-    print(res.text)
+    soup = BeautifulSoup(res.text, 'html.parser')
+    print(soup.find('div'))
